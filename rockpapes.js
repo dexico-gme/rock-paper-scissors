0b59a6540fb7e9f8c85ee3b3e7db8e42408ea31c
Dexter Kong <78414602+dexico-gme@users.noreply.github.com>
2023-05-21 16:44:58 +0100

    modified:   rockpapes.js
    updated the functions playRound & getComputer choice.
    user prompt integrated

--- rockpapes.js
+++ rockpapes.js
@@ -1,10 +1,32 @@
+// function to randomly choose rock, paper or scissors
 let getComputerChoice = function () {
-  let arr = ["Rock", "Paper", "Scissors"];
+  let arr = ["rock", "paper", "scissors"];
   let randNum = Math.floor(Math.random() * arr.length);
-  console.log(arr[randNum]);
+  return arr[randNum];
 };
 getComputerChoice();
 
+// function to play a round of rock paper scissors
 function playRound(playerSelection, computerSelection) {
-  return `You Lose! ${playerSelection} beats ${computerSelection}`;
+  //console.log(computerSelection);
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+  } else if (playerSelection === computerSelection) {
+    console.log(`It's a draw!`);
+  } else {
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+  }
 }
+
+let playerSelection = prompt("user input: ").toLowerCase();
+let computerSelection = getComputerChoice();
+
+playRound(playerSelection, computerSelection);
+
+// rock beats scissors
+// scissors beats paper
+// paper beats rock
